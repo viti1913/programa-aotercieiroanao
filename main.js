cccd826b472592934fd16faf6b194e7c1a347154
viti1913 <106247419+viti1913@users.noreply.github.com>
2024-04-09 15:32:53 -0300

    penis1

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
-const botoes = document.querySelectorAll(".botao"); //esta linha seleciona todos os elementos HTML que tem a classe 'botao' e armazena-os na constante 'botoes'//
-for (let i = 0; i < botoes.length; i++) { //este é um loop que percorre cada botao na constante 'botoes'//
-    botoes[i].onclick = function (index){ //aqui, estamos atribuindo uma função ao evento 'onclick' de cada botão. Isso significa que a função será executada sempre que o botão for clicado//
-        for (let j = 0; j < botoes.length; j++) { //dentro da função 'onclick', temos outro loop que percorre cada botão novamente*/
-            botoes[j].classList.remove("ativo"); //aqui removemos a classe 'ativo' de cada botão. Isso pode ser usado para alterar o estilo do botão, por exemplo, para indicar qual botão está atualmente ativo//
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function (){
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");        
+            textos[j].classList.remove("ativo");    
         }
-        botoes[i].classList.add("ativo"); //depois de remover a classe 'ativo' de todos os botões, adicionamos a classe 'ativo' de volta ao botão que foi clicado. Isso efetivamente garante que apenas o botão clicado tenha a classe 'ativo'//
+        botoes[i].classList.add("ativo");   
+        textos[i].classList.add("ativo");
     }
+}
+const contadores = document.querySelectorAll(".contador");
+const tempo1 = new Date("2024-12-13T00:00:00");
+
+contadores[0].textContent = calculaTempo(tempo1);
+function calculaTempo(tempo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
 }
